d5be254c3d6098f619beaf63b005e6e753fd85fd
degeneratezan <noxindocraft@gmail.com>
2022-09-01 11:06:31 +0700

    fix media screen on mobile

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () { 
+    if(window.innerWidth < 640){
+        alert("aww.. looks like you're on mobile mode, please switch to desktop mode for greater looks :)")
+    };
+ })
